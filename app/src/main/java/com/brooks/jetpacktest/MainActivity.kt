653124ff27815f9ac5faeda92ea01b9578aae88c
brooks <myt2000@126.com>
2023-01-04 14:06:16 +0800

    实现页面主要功能

--- app/src/main/java/com/brooks/jetpacktest/MainActivity.kt
+++ app/src/main/java/com/brooks/jetpacktest/MainActivity.kt
@@ -2,10 +2,27 @@ package com.brooks.jetpacktest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        plusOneBtn.setOnClickListener {
+            viewModel.counter++
+            refreshCounter()
+        }
+        refreshCounter()
+    }
+
+    private fun refreshCounter() {
+        infoText.text = viewModel.counter.toString()
     }
 }
