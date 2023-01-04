53c79625e2039834837717aecbed9f2edc77350e
brooks <myt2000@126.com>
2023-01-04 15:11:31 +0800

    使用SharedPreferences键值对存储数据

--- app/src/main/java/com/brooks/jetpacktest/MainActivity.kt
+++ app/src/main/java/com/brooks/jetpacktest/MainActivity.kt
@@ -1,25 +1,47 @@
 package com.brooks.jetpacktest
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.edit
 import androidx.lifecycle.ViewModelProvider
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var viewModel: MainViewModel
+    lateinit var sp: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        sp = getPreferences(Context.MODE_PRIVATE)
+        val countReserved = sp.getInt("count_reserved", 0)
 
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        viewModel = ViewModelProvider(this, MainViewModelFactory(countReserved)).get(MainViewModel::class.java)
+
+
+
+//        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
 
         plusOneBtn.setOnClickListener {
             viewModel.counter++
             refreshCounter()
         }
+
+        clearBtn.setOnClickListener {
+            viewModel.counter = 0
+            refreshCounter()
+        }
         refreshCounter()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sp.edit {
+            putInt("count_reserved", viewModel.counter)
+        }
     }
 
     private fun refreshCounter() {
