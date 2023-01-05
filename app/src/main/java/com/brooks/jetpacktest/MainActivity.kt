4700163a154e40b4938a938198c7cd5c69e04d4e
brooks <myt2000@126.com>
2023-01-05 16:40:39 +0800

    MyObserver感知Activity生命周期

--- app/src/main/java/com/brooks/jetpacktest/MainActivity.kt
+++ app/src/main/java/com/brooks/jetpacktest/MainActivity.kt
@@ -48,6 +48,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        lifecycle.addObserver(MyObserver())
         sp = getPreferences(Context.MODE_PRIVATE)
         val countReserved = sp.getInt("count_reserved", 0)
 
