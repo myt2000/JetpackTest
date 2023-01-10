41f4de645d153d2781ff642a36770bad69d83b27
brooks <myt2000@126.com>
2023-01-10 15:58:26 +0800

    新一个User数据类，另外建立User的livedata

--- app/src/main/java/com/brooks/jetpacktest/MainViewModel.kt
+++ app/src/main/java/com/brooks/jetpacktest/MainViewModel.kt
@@ -2,11 +2,22 @@ package com.brooks.jetpacktest
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 
 class MainViewModel(countReserved: Int): ViewModel() {
+
+    private val userLiveData = MutableLiveData<User>()
+
+    val userName: LiveData<String> = Transformations.map(userLiveData) {
+        user -> "${user.firstName} ${user.lastName}"
+    }
+
+
     var counter: LiveData<Int> = MutableLiveData()
         get() = _counter
+
+
 
     private var _counter = MutableLiveData<Int>()
 
