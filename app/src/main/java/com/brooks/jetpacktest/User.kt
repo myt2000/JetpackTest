bde66a2b673005b439e7e1c0c62df94d10884d3b
brooks <myt2000@126.com>
2023-01-11 10:33:02 +0800

    使用room的orm，UserDao实现相关orm接口，实现增删改查

--- app/src/main/java/com/brooks/jetpacktest/User.kt
+++ app/src/main/java/com/brooks/jetpacktest/User.kt
@@ -1,4 +1,11 @@
 package com.brooks.jetpacktest
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
 data class User(var firstName: String, var lastName: String, var age: Int) {
+
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0
 }
