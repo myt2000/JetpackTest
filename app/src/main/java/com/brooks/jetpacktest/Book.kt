83ac00fef0101264035919c3734a715ac76093f8
brooks <myt2000@126.com>
2023-01-11 15:13:36 +0800

    创建Book数据类和Dao层

---
+++ app/src/main/java/com/brooks/jetpacktest/Book.kt
@@ -0,0 +1,11 @@
+package com.brooks.jetpacktest
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+ data class Book(var name: String, var pages: Int) {
+
+     @PrimaryKey(autoGenerate = true)
+     var id: Long = 0
+}
---
+++ app/src/main/java/com/brooks/jetpacktest/BookDao.kt
@@ -0,0 +1,15 @@
+package com.brooks.jetpacktest
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+
+@Dao
+interface BookDao {
+    @Insert
+    fun insertBook(book: Book): Long
+
+    @Query("select * from Book")
+    fun loadAllBooks(): List<Book>
+}
