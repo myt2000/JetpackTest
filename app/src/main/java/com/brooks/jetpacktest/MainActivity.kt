661c2e6766143c1abe2d9f8362ae636f577717be
brooks <myt2000@126.com>
2023-01-11 15:00:01 +0800

    添加获取数据库路径的代码和日志打印

--- app/src/main/java/com/brooks/jetpacktest/MainActivity.kt
+++ app/src/main/java/com/brooks/jetpacktest/MainActivity.kt
@@ -4,42 +4,13 @@ import android.content.Context
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
+import android.util.Log
 import androidx.core.content.edit
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.concurrent.thread
 
-//class MyObserver {
-//    fun activityStart() {
-//
-//    }
-//
-//    fun activityStop() {
-//
-//    }
-//}
-//
-//
-//class MainActivity  : AppCompatActivity() {
-//
-//    lateinit var observer: MyObserver
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        observer = MyObserver()
-//    }
-//
-//    override fun onStart() {
-//        super.onStart()
-//        observer.activityStart()
-//    }
-//
-//    override fun onStop() {
-//        super.onStop()
-//        observer.activityStop()
-//    }
-//}
 
 class MainActivity : AppCompatActivity() {
 
@@ -77,6 +48,40 @@ class MainActivity : AppCompatActivity() {
         viewModel.counter.observe(this, Observer { count ->
             infoText.text = count.toString()
         })
+
+        val userDao = AppDatabase.getDatabase(this).userDao()
+        val user1 = User("Tom", "Brady", 40)
+        val user2 = User("Tom", "Hanks", 63)
+
+        addDataBtn.setOnClickListener {
+            thread {
+                user1.id = userDao.insertUser(user1)
+                user2.id = userDao.insertUser(user2)
+            }
+        }
+
+        updateDataBtn.setOnClickListener {
+            thread {
+                user1.age = 42
+                userDao.updateUser(user1)
+            }
+        }
+
+        deleteDataBtn.setOnClickListener {
+            thread {
+                userDao.deleteUserByLastName("Hanks")
+            }
+        }
+
+        queryDataBtn.setOnClickListener {
+            thread {
+                for (user in userDao.loadAllUsers()) {
+                    Log.d("MainActivity", user.toString())
+                }
+                var path = getDatabasePath("app_database").getAbsolutePath();
+                Log.d("数据库路径", "$path")
+            }
+        }
     }
 
     override fun onPause() {
